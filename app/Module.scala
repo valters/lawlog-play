356b200c400da497a7e4029ccf4ef5af4ec5105b
Valters Vingolds <valters@vingolds.ch>
2017-01-26 15:43:17 +0100

    app seed: remove unused components/routes

--- app/Module.scala
+++ app/Module.scala
@@ -1,7 +1,5 @@
 import com.google.inject.AbstractModule
-import java.time.Clock
 
-import services.{ApplicationTimer, AtomicCounter, Counter}
 import services.TableOfContents
 import services.JsonTableOfContents
 
@@ -18,15 +16,9 @@ import services.JsonTableOfContents
 class Module extends AbstractModule {
 
   override def configure() = {
-    // Use the system clock as the default implementation of Clock
-    bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
-    // Ask Guice to create an instance of ApplicationTimer when the
-    // application starts.
-    bind(classOf[ApplicationTimer]).asEagerSingleton()
-    // Set AtomicCounter as the implementation for Counter.
-    bind(classOf[Counter]).to(classOf[AtomicCounter])
 
     bind(classOf[TableOfContents]).to(classOf[JsonTableOfContents])
+
   }
 
 }
