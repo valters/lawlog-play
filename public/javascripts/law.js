39c4682abece6935da83378b50d0e423ddadf312
Valters Vingolds <valters@vingolds.ch>
2017-03-07 17:52:24 +0100

    The 'active' version is toggled on menu links, as law is loaded

--- public/javascripts/law.js
+++ public/javascripts/law.js
@@ -2,26 +2,38 @@ var law = new Vue({
   el: '#lawdiff-ajax-content',
 
   data: {
-    body_content: "Loading..."
-  },
-
-  mounted: function() {
-    console.log("IN VUE")
-
-    this.loadLaw( "satversme", "20140722" )
+    fromAjax: false,
+    bodyContent: "Loading...",
+    currVer: null
   },
 
   methods: {
     loadLaw: function ( law, ver ) {
+        this.toggleVer()
+        this.currVer = ver
+        this.toggleVer()
+
+        this.fromAjax = true
+
         var url = "/raw/likums/"+law+"/v/"+ver
+        var href = "/likums/"+law+"/v/"+ver
 
         console.log("loadLaw: GET " + url )
 
         var vue = this
         $.get( url, function(data) {
-            vue.body_content = data
+            vue.bodyContent = data
+
+            window.history.pushState( null, null, href )
         })
+    },
+
+    toggleVer: function() {
+        if( this.currVer ) {
+            $("#"+this.currVer).toggleClass("active");
+        }
     }
+
   }
 
 })
