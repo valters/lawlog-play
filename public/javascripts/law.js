f7b9d0516bdf73cef4e8a747b9f7f38fb4f7eaf7
Valters Vingolds <valters@vingolds.ch>
2017-03-08 09:07:18 +0100

    SEO: don't rely on javascript to provide initial content on page - keep pre-rendered server side stuff (and also don't strictly require JS)

--- public/javascripts/law.js
+++ public/javascripts/law.js
@@ -2,36 +2,52 @@ var law = new Vue({
   el: '#lawdiff-ajax-content',
 
   data: {
-    fromAjax: false,
-    bodyContent: "Loading...",
+    bodyContent: null,
     currVer: null
   },
 
   methods: {
+    // seed with current version
+    seedLaw: function( ver ) {
+        this.currVer = ver
+        console.log("seedLaw: " + this.currVer )
+    },
+
     loadLaw: function ( law, ver ) {
+        console.log("loadLaw: " + this.currVer + " -> " + ver )
+        if( this.currVer == ver ) {
+            return // do nothing
+        }
+
         this.toggleVer()
         this.currVer = ver
         this.toggleVer()
 
-        this.fromAjax = true
-
-        var url = "/raw/likums/"+law+"/v/"+ver
-        var href = "/likums/"+law+"/v/"+ver
+        var url = '/raw/likums/'+law+'/v/'+ver
+        var href = '/likums/'+law+'/v/'+ver
 
         console.log("loadLaw: GET " + url )
 
         var vue = this
         $.get( url, function(data) {
+            vue.hideServerSideContent()
+
             vue.bodyContent = data
 
             window.history.pushState( null, null, href )
         })
     },
 
+    // mark (or unmark) active law version css
     toggleVer: function() {
         if( this.currVer ) {
-            $("#"+this.currVer).toggleClass("active");
+            $('#'+ this.currVer ).toggleClass('active');
         }
+    },
+
+    // before we do ajax replace, we want to hide something that was pre-rendered server side
+    hideServerSideContent: function() {
+        $('#lawdiff-server-side').hide()
     }
 
   }
